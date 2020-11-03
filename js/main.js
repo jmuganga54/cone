34672e63392ddb04ee64c53456e96e5ed249f658
joseph_muganga <jmuganga544@gmail.com>
2020-11-03 11:49:26 +0300

    Adding manifest.json

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-//Make sure sw is working
-if('serviceWorker' in navigator){
-    //We need to register service worker when the application is on load
-    window.addEventListener('load',()=>{
-      navigator.serviceWorker 
-      .register('../sw.js') //registering service worker
-      .then(reg => console.log('Service Worker: Registered')) // if service worker registered successfull display
-      .catch(error => console.log('Service Worker: Error: ${error}'))//if not display error
-    })
-  }
-  
+//To install a service worker you need to kick start the process by registering it in your page
+//This tells the browser where your service worker Javascript files lives
+//One subtlety with the [register()] method is the location of the srvice worker file.You'll notice in this case that the service worker file is at the root of the domain.
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load',()=> {
+    navigator.serviceWorker.register('../sw.js').then((registration)=> {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, (err)=> {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
   /*Caching all the resources on a browse */
-  const cacheName = 'v1'; //cache name
+  const cacheName = 'cone-bioassay-v1'; //cache name
 
-  //creating an array to be cached which contain all the resources
-  const cacheAssets = [ 
+  //creating an array which contain all the resources to be cached
+  const assetsToCache = [ 
   '/css/theme-grid.css',
   '/css/theme-main.css',
   '/css/customized.css',
@@ -11,62 +11,74 @@
   '/fonts/OpenSans-Regular-webfont.woff',
   '/images/icon_180x180.png',
   'js/main.js',
-  'index.html',
-
-
-
+  'index.html'
 ];
  
  
  //Call Install Event
- self.addEventListener('install', (e)=>{
+ self.addEventListener('install', (event)=>{
      console.log('Service Worker: Installed'); 
      
-     //telling the browser to wait until our promise is finished getting ready with service worker
-     //using cache storage api, caches.open(cacheName) which open the cache
-     //output when the caches api have the name
-     //adding all the resources in a cache
-     //if all the resources have been added skip waiting
+  /*Inside of our install callback, we neeed to take the following steps:
+   1:Open a cache, 2.Cache our files 3. Confirm whether all the required assets are cached or not */
 
-     e.waitUntil(
-         caches
-         .open(cacheName)
-         .then(cache =>{
-             console.log('Service Worker: Caching Files');
-             cache.addAll(cacheAssets);
+   //Perform install steps
+     event.waitUntil(
+         caches.open(cacheName)
+         .then((cache) =>{
+             console.log('Opened Cache');
+             return cache.addAll(assetsToCache);
+
+             /*NB Here you can see we call [caches.open()] with our desired cache name, after which we call [cache.addAll()] and pass in our array of files.This is a chain of promises [(cache.open())] and [cache.addAll()].The event.waitUntill() method takes a promise and uses it to know how long installation takes, and whether it succeeded or not.*/
              
          })
-         .then(()=>self.skipWaiting())
+        //  .then(()=>self.skipWaiting())
      )
 
  });
 
- //Call Activate Event
- self.addEventListener('activate', e=>{
-     console.log('Service Workder: Activated');
+  //Call Activate Event
+  self.addEventListener('activate', (event)=>{
+    console.log('Service Workder: Activated');
 
-     //Remove unwanted caches
-     //Looping the array of all caches and remove all the caches which are not required
-     e.waitUntil(
-         caches.keys().then(cacheNames =>{
-             return Promise.all(
-                 cacheNames.map(cache =>{
-                    if(cache != cacheName){
-                        console.log('Service Worker: Clearing Old Cache');
-                        return caches.delete(cache);
-                    }
-                 })
-             )
-         })
-     )
-      
- });
+    //Remove unwanted caches
+    //Looping the array of all caches and remove all the caches which are not required
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>{
+            return Promise.all(
+                cacheNames.map(cache =>{
+                   if(cache != cacheName){
+                       console.log('Service Worker: Clearing Old Cache');
+                       return caches.delete(cache);
+                   }
+                })
+            )
+        })
+    )
+     
+});
 
  //Call Fetch Event
- self.addEventListener('fetch', e=>{
-     console.log('Service Worker: Fetching');
+ self.addEventListener('fetch', (event) =>{
+    console.log('Service Worker: Fetching');
 
-     //First check if the live site is available, then if not we want to load the cache site
-     e.respondWith(
-         fetch(e.request).catch(()=> caches.match(e.request)));
- })
+    //First check if the live site is available, then if not we want to load the cache site
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) =>{
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+
+    /*NB Here we've defined our fetch event and within [event.respondWith()], we pass in a promise from caches.match().This method looks at the request an finds any cached results from any of the caches your service worker created. 
+    
+    If we have a matching response, we return the cached value, otherwise we return the result of a call to fetch, which will make a network request and return the data if anything can be retrieved from the network. This is a simple example and uses any cached assets we cached during the install step.
+    */
+  });
+
+
